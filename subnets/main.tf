9294e3b01d0729c217ad4f04a16b460a25715c99
balatuluva <bala.tulva@gmail.com>
2024-02-17 07:41:11 +0000

    setup route table for vpc

--- subnets/main.tf
+++ subnets/main.tf
@@ -3,6 +3,11 @@ resource "aws_subnet" "main" {
   vpc_id = var.vpc_id
   cidr_block = var.cidr_block[count.index]
   availability_zone = var.azs[count.index]
-
   tags = merge(var.tags, { Name = "${var.env}-${var.name}-subnet-${count.index+1}" })
+}
+
+resource "aws_route_table" "main" {
+  count = length(var.cidr_block)
+  vpc_id = var.vpc_id
+  tags = merge(var.tags, { Name = "${var.env}-${var.name}-rt-${count.index+1}" }
 }
